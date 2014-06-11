ad81d95431eb0aee0189017140ad2272339817da
Seba Gamboa <me@sagmor.com>
2014-06-11 17:00:45 -0400

    Example project scaffold

--- example/config/initializers/secret_token.rb
+++ example/config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Example::Application.config.secret_token = '1cc6e9e77e207358658dcf488727b830a3cbd4fcc5a525bc035ef717e183249645e78a748591ad4629e7e1e0dad660a70e2b5c843659f34c8924d413aaf92dbe'
+Example::Application.config.secret_key_base = '1cc6e9e77e207358658dcf488727b830a3cbd4fcc5a525bc035ef717e183249645e78a748591ad4629e7e1e0dad660a70e2b5c843659f34c8924d413aaf92dbe'
